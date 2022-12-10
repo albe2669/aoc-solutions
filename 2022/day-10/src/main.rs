57200c3a426bf8511431535019ea282f909f2df9
Albert Rise Nielsen <albert@risenielsen.dk>
2022-12-10 18:33:07 +0100

    2022-day-10 cleaner solution

--- 2022/day-10/src/main.rs
+++ 2022/day-10/src/main.rs
@@ -1,11 +1,69 @@
-use std::{cell::RefCell, collections::HashMap, env, fs, process::exit, rc::Rc};
+use std::{env, fs};
 
+#[derive(Clone, Debug)]
 enum Operation {
     Addx(i32),
+    Aaddx(i32),
     Nop,
+    NextOp(usize),
 }
 
 type Input = Vec<Operation>;
+
+struct CPU {
+    ops: Vec<Operation>,
+    stack: Vec<Operation>,
+    x: i32,
+    cycle: i32,
+    done: bool,
+}
+
+impl CPU {
+    fn new(n: &Input) -> Self {
+        let mut c = Self {
+            ops: n.clone(),
+            stack: Vec::new(),
+            x: 1,
+            cycle: 1,
+            done: false,
+        };
+
+        c.stack.push(Operation::NextOp(0));
+        c
+    }
+
+    fn simulate_cycle(&mut self) {
+        let op = self.stack.pop().unwrap();
+        self.cycle += 1;
+
+        match op {
+            Operation::Nop => (),
+            Operation::Addx(_) => panic!("Addx should not be on the stack"),
+            Operation::Aaddx(v) => self.x += v,
+            Operation::NextOp(i) => {
+                self.cycle -= 1;
+                let new_op = self.ops.get(i as usize);
+                match new_op {
+                    Some(Operation::Addx(x)) => {
+                        self.stack.push(Operation::NextOp(i + 1));
+                        self.stack.push(Operation::Aaddx(*x));
+                        self.stack.push(Operation::Nop);
+                    }
+                    Some(v) => {
+                        self.stack.push(Operation::NextOp(i + 1));
+                        self.stack.push(v.clone());
+                    }
+                    None => {
+                        self.done = true;
+                        return;
+                    }
+                }
+
+                self.simulate_cycle();
+            }
+        }
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -31,77 +89,43 @@ fn main() {
 }
 
 fn part1(n: &Input) {
+    let mut cpu = CPU::new(n);
     let mut sum = 0;
-    let mut x = 1;
-    let mut current_cycle = 0;
-    let mut current_op = 0;
-    let mut current_op_rem = 0;
 
-    while current_op < n.len() {
-        current_cycle += 1;
+    while !cpu.done {
         match [20, 60, 100, 140, 180, 220]
             .iter()
-            .find(|&&x| x == current_cycle)
+            .find(|&&x| x == cpu.cycle)
         {
-            Some(v) => sum += x * v,
+            Some(v) => sum += cpu.x * v,
             None => (),
         };
 
-        match n[current_op] {
-            Operation::Nop => {
-                current_op += 1;
-            }
-            Operation::Addx(_) if current_op_rem == 0 => {
-                current_op_rem = 1;
-            }
-            Operation::Addx(v) if current_op_rem == 1 => {
-                x += v;
-                current_op_rem = 0;
-                current_op += 1;
-            }
-            _ => panic!("Unknown operation, {}", current_op),
-        }
+        cpu.simulate_cycle();
     }
 
     println!("Part 1: {}", sum);
 }
 
 fn part2(n: &Input) {
+    let mut cpu = CPU::new(n);
     let mut rows: Vec<Vec<char>> = Vec::new();
-    let mut x: i32 = 1;
     let mut current_row = -1;
-    let mut current_cycle = 0;
-    let mut current_op = 0;
-    let mut current_op_rem = 0;
 
-    while current_op < n.len() {
-        current_cycle += 1;
-
-        let crt_location = (current_cycle - 1) % 40;
+    while !cpu.done {
+        let crt_location = (cpu.cycle - 1) % 40;
         if crt_location == 0 {
             rows.push(vec!['\u{2591}'; 40]);
             current_row += 1;
         }
-        if crt_location == x - 1 || crt_location == x + 1 || crt_location == x {
+        if crt_location == cpu.x - 1 || crt_location == cpu.x + 1 || crt_location == cpu.x {
             rows[current_row as usize][crt_location as usize] = '\u{2588}';
         }
 
-        match n[current_op] {
-            Operation::Nop => {
-                current_op += 1;
-            }
-            Operation::Addx(_) if current_op_rem == 0 => {
-                current_op_rem = 1;
-            }
-            Operation::Addx(v) if current_op_rem == 1 => {
-                x += v;
-                current_op_rem = 0;
-                current_op += 1;
-            }
-            _ => panic!("Unknown operation, {}", current_op),
-        }
+        cpu.simulate_cycle();
     }
 
+    println!("Part 2:");
     for row in rows {
         println!("{}", row.iter().collect::<String>());
     }
