c9d184cca9107f78d54fff44a8ebbd06d2da1823
Albert Rise Nielsen <albert@risenielsen.dk>
2024-12-03 21:36:04 +0100

    2024-meta: steal alot of code

---
+++ 2024/src/aoc/utils.clj
@@ -0,0 +1,24 @@
+#_{:clj-kondo/ignore [:namespace-name-mismatch]}
+(ns aoc.utils
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]))
+
+(defn to-lines [input]
+  (str/split-lines input))
+
+(defn parse-longs [line]
+  (mapv parse-long (re-seq #"\d+" line)))
+
+;; Stolen from: https://github.com/rjray/advent-2024-clojure/blob/master/src/advent_of_code/utils.clj
+(defn read-input
+  "Read in the content of the given day-file and return as a blob"
+  [day]
+  (slurp (if (str/starts-with? day "/") day (io/resource day))))
+
+;; Like the core time macro, but rather than printing the elapsed time it
+;; returns a list of (result, time). Returned value is in milliseconds.
+(defmacro time-it [expr]
+  `(let [start# (. System (nanoTime))
+         ret#   ~expr
+         end#   (/ (double (- (. System (nanoTime)) start#)) 1000000.0)]
+     (list ret# end#)))
